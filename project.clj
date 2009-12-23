c6832c68f83a4c9490296b23884b4f82cfc2dd8d
Kevin Downey <redchin@gmail.com>
2009-12-23 15:33:16 -0800

    procject.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject hiredman.repl "0.5.0-SNAPSHOT"
+  :description "A build tool designed not to set your hair on fire."
+  :url "http://github.com/hiredman/repl"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
