4eb2fabfac543cbafa3f6fe8958695acc763ab88
Kevin Downey <redchin@gmail.com>
2009-12-24 22:21:35 -0800

    change name of repo to reflect reality

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hiredman.repl "0.0.1-SNAPSHOT"
   :description ""
-  :url "http://github.com/hiredman/repl"
+  :url "http://github.com/hiredman/Repl"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
